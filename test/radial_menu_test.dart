aeef9846ce7a39d758ec40ea413e16a8a74c057d
Hugo Heneault <hugo@ownweb.fr>
2020-08-06 18:34:47 +0200

    ✅(tests): Remove default tests until we create some

--- test/radial_menu_test.dart
+++ test/radial_menu_test.dart
@@ -1,23 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:radial_menu/radial_menu.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('radial_menu');
-
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await RadialMenu.platformVersion, '42');
-  });
 }
